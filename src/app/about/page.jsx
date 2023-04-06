1c3975e4e83f366a2f1625accb24d4743cb1d1ba
Ghazan Khan <ghazan.elitx@gmail.com>
2023-04-06 11:50:57 +0500

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const about = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our IMDb clone website, a platform designed for movie and TV
+        enthusiasts around the world.Our website aims to provide you with the
+        most comprehensive database of movies, TV shows, actors, and other
+        entertainment-related content, all in one place.
+      </p>
+      <p>
+        We understand that keeping up with the latest movies, TV shows, and
+        entertainment news can be overwhelming. That's why our platform is
+        designed to make it easy for you to discover new content, find
+        information about your favorite movies and TV shows, and stay up-to-date
+        with the latest industry news.
+      </p>
+      <p>
+        Our team is passionate about movies and TV shows, and we believe that
+        everyone should have access to a platform that makes it easy to explore
+        the world of entertainment. We are committed to creating a
+        user-friendly, reliable, and up-to-date database that provides our users
+        with accurate and comprehensive information about their favorite movies,
+        TV shows, and actors.{" "}
+      </p>
+      <p>
+        We believe that movies and TV shows are more than just entertainment;
+        they have the power to inspire, educate, and unite people from all walks
+        of life. Our mission is to create a platform that celebrates the power
+        of movies and TV shows to bring people together, spark conversations,
+        and inspire change.
+      </p>
+    </div>
+  );
+};
+
+export default about;
