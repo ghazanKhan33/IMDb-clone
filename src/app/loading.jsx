c9b0907ccfc0b068c6843b3fd251514a96bc0642
Ghazan Khan <ghazan.elitx@gmail.com>
2023-04-06 14:27:47 +0500

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center mt-10'>
+        <img src="infinity.svg" alt="loading..." />
+    </div>
+  )
+}
+
+export default Loading
