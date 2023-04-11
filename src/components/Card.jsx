6598993954279063d3f985aedf8ec29af5f45b8b
Ghazan Khan <ghazan.elitx@gmail.com>
2023-04-11 12:05:06 +0500

    added the movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,28 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import React from "react";
 import { FiThumbsUp } from "react-icons/fi";
+import Image from "next/image";
 
 const Card = ({ result }) => {
   return (
     <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
       <Link href={`/movie/${result.id}`}>
-        <img
+        <Image
           style={{ maxWidth: "100%", height: "auto" }}
           className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
-          src={`https://image.tmdb.org/t/p/original/${
+          src={`https://image.tmdb.org/t/p/original${
             result.backdrop_path || result.poster_path
           }`}
-          alt={result.original_title}
+          alt="image not available"
+          width={500}
+          height={300}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
         />
         <div className="p-2">
-          <p className="line-clamp-3">{result.overview}</p>
+          <p className="line-clamp-2">{result.overview}</p>
           <h2 className="truncate text-xl font-bold">
-            {result.title || result.name} 
+            {result.title || result.name}
           </h2>
           <p className="text-sm flex items-center">
             {result.release_date || result.first_air_date}
